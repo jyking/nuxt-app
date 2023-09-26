7ef65945f4c9551ac1170c9ed251c00c3af985ae
jyking <jyking@qq.com>
2023-09-26 22:41:22 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
-        <div class="header"></div>
+    <Html class="h-full">
+        <Body class="flex h-full"></Body>
+    </Html>
+    
+    <div class="flex flex-row h-full bg-gray-300 text-gray-600">
+        <leftSidebar />
         <div class="body ">
-            <div class="left-sidebar "></div>
-            <div class="main "></div>
+            <div class="header">网址导航</div>
+            <div class="main ">333</div>
         </div>
     </div>
 </template>
+
+<script setup>
+const title = ref('Hello World')
+</script>
