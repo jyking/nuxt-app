8235c3370d03f0785b9a52266062fc2f18bd4737
jyking <jyking@qq.com>
2023-10-26 09:13:02 +0800

    init

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,38 @@
 <template>
-    <section>
-        <p>This page will be displayed at the /about route.</p>
-    </section>
+<div class="flex h-full flex-wrap bg-gray-200">
+  <div class="my-2 h-20 w-1/2 border-gray-700 md:w-1/3">
+    <div class="m-2 flex h-full border bg-yellow-100">
+      <div class="my-auto mr-5 w-10">
+        <img src="https://api.iowen.cn/favicon/www.grabient.com.png" />
+      </div>
+      <div class="my-auto flex h-full w-full flex-col border">
+        <div class="text-sm">Sketchpacks</div>
+        <div class="overflow-hidden text-xs text-gray-400">跨团队管理和同步sketch插件工具</div>
+      </div>
+    </div>
+  </div>
+  <div class="my-2 h-20 w-1/2 border-gray-700 md:w-1/3">
+    <div class="m-2 flex h-full border bg-yellow-100">
+      <div class="my-auto mr-5 w-10">
+        <img src="https://api.iowen.cn/favicon/www.grabient.com.png" />
+      </div>
+      <div class="my-auto flex h-full w-full flex-col border">
+        <div class="text-sm">Sketchpacks</div>
+        <div class="overflow-hidden text-xs text-gray-400">跨团队管理和同步sketch插件工具</div>
+      </div>
+    </div>
+  </div>
+  <div class="my-2 h-20 w-1/2 border-gray-700 md:w-1/3">
+    <div class="m-2 flex h-full border bg-yellow-100">
+      <div class="my-auto mr-5 w-10">
+        <img src="https://api.iowen.cn/favicon/www.grabient.com.png" />
+      </div>
+      <div class="my-auto flex h-full w-full flex-col border">
+        <div class="text-sm">Sketchpacks</div>
+        <div class="overflow-hidden text-xs text-gray-400">跨团队管理和同步sketch插件工具</div>
+      </div>
+    </div>
+  </div>
+</div>
+
 </template>
